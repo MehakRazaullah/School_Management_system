e0b5d702adc920dfbe94920edaa1cd83dc96b98c
Mehak Nizamani <105057287+MehakRazaullah@users.noreply.github.com>
2022-10-26 20:25:48 +0500

    Add files via upload

--- Student_Management.java
+++ Student_Management.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.lang.Thread;
 import java.lang.Exception;
-import java.io.Serializable;
+import java.util.Random;
 
-class Student implements Serializable{
+class Student implements Serializable {
     int id;
     String name,contact_no,email;
     double fee;
@@ -17,34 +17,35 @@ class Student implements Serializable{
         this.email=email;
         this.fee=fee;
     }
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String RESET = "\u001B[0m";
+    public static final String PURPLE = "\u001B[35m";
 
+    //autocall
     public String toString(){
-        return ( ANSI_PURPLE +"\n Student Details:  "+"\n ID: "+id+"\n Student Name: "+name+
-                "\n Contact No: "+contact_no+"\n Semester fee:  "+fee+"\n Email Address: "+email +" "+ANSI_RESET);
+        return ( PURPLE +"\n Student Details:  "+"\n ID: "+id+"\n Student Name: "+name+
+                "\n Contact No: "+contact_no+"\n Semester fee:  "+fee+"\n Email Address: "+email +" "+RESET);
     }
 
 
 }
 class Loading extends Thread{
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String RESET = "\u001B[0m";
+    public static final String YELLOW = "\u001B[33m";
     public void run(){
         try {
-            System.out.println(ANSI_YELLOW +"                                L"+ANSI_RESET );
+            System.out.println(YELLOW +"                                L"+RESET );
             Thread.sleep(1000);
-            System.out.println(ANSI_YELLOW +"                                O"+ANSI_RESET );
+            System.out.println(YELLOW +"                                O"+RESET );
             Thread.sleep(1000);
-            System.out.println(ANSI_YELLOW +"                                A"+ANSI_RESET );
+            System.out.println(YELLOW +"                                A"+RESET );
             Thread.sleep(1000);
-            System.out.println(ANSI_YELLOW +"                                D"+ANSI_RESET );
+            System.out.println(YELLOW +"                                D"+RESET );
             Thread.sleep(1000);
-            System.out.println(ANSI_YELLOW +"                                I"+ANSI_RESET );
+            System.out.println(YELLOW +"                                I"+RESET );
             Thread.sleep(1000);
-            System.out.println(ANSI_YELLOW +"                                N"+ANSI_RESET );
+            System.out.println(YELLOW +"                                N"+RESET );
             Thread.sleep(1000);
-            System.out.println(ANSI_YELLOW +"                                G"+ANSI_RESET );
+            System.out.println(YELLOW +"                                G"+RESET );
         }
         catch (Exception e){
             System.out.println(e);
@@ -53,27 +54,25 @@ class Loading extends Thread{
 }
 class Student_Management1{
     // Declaring ANSI_RESET so that we can reset the color
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_BLUE  = "\u001B[34m";
+    public static final String RESET = "\u001B[0m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String BLUE  = "\u001B[34m";
 
-static void display(ArrayList<Student>emp){
-
-        System.out.println(ANSI_PURPLE  +"\n          *STUDENTS LIST**\n" + ANSI_RESET);
-        for(Student e:emp){
-        System.out.println(ANSI_BLUE +"\n Student Details:  "+"\n ID: "+e.id+"\n Student Name: "+e.name+
-        "\n Contact No: "+e.contact_no+"\n Semester fee:  "+e.fee+"\n Email Address: "+e.email + ANSI_RESET);
+ void display(ArrayList<Student>Stud){
+        System.out.println(PURPLE  +"\n         **STUDENTS LIST**\n" + RESET);
+        for(Student e:Stud){
+        System.out.println(BLUE +"\n Student Details:  "+"\n ID: "+e.id+"\n Student Name: "+e.name+
+        "\n Contact No: "+e.contact_no+"\n Semester fee:  "+e.fee+"\n Email Address: "+e.email + RESET);
         }
 }
 }
 public class Student_Management {
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String  ANSI_RED = "\u001B[31m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_BLUE  = "\u001B[34m";
-
-    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String RESET = "\u001B[0m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String  RED = "\u001B[31m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String BLUE  = "\u001B[34m";
+    public static final String YELLOW = "\u001B[33m";
 
     public static void main(String[] args) {
 
@@ -82,13 +81,15 @@ public class Student_Management {
 
         Student_Management1 m = new Student_Management1();
 
-        int id;
+       int id;
         String name,contact_no,email;
         double fee;
 
+        Random rand = new Random();
 
         Scanner sc=new Scanner(System.in);
-        ArrayList<Student>emp=new ArrayList<>();
+
+        ArrayList<Student>Stud=new ArrayList<>();
 
 
         File f=null;
@@ -96,14 +97,19 @@ public class Student_Management {
         ObjectInputStream ois=null;
         FileOutputStream fos=null;
         ObjectOutputStream oos=null;
+
+
         try {
             f=new File("new.txt");
             if(f.exists()){
                 fis=new FileInputStream(f);
                 ois=new ObjectInputStream(fis);
-                emp=(ArrayList<Student>)ois.readObject();
+
+                //deseralization
+                Stud=(ArrayList<Student>)ois.readObject();
 
             }
+
 
         }catch (Exception e){
             System.out.println(e);
@@ -111,71 +117,105 @@ public class Student_Management {
 
         do {
 
-            System.out.println( ANSI_CYAN+"\n                  *** STUDENTS MANAGEMENT SYSTEM ***                  \n"+ANSI_RESET);
+            System.out.println( CYAN+"\n                  *** STUDENTS MANAGEMENT SYSTEM ***                  \n"+RESET);
 
-            System.out.println(  ANSI_PURPLE+"                  -------------------------------------------                  "+ANSI_RESET);
+            System.out.println( PURPLE+"                  -------------------------------------------                  "+RESET);
             System.out.println(
-                    ANSI_CYAN+"                  1) Add Student data                  \n"+ANSI_RESET
-                    + ANSI_PURPLE+ "                  -------------------------------------------                  \n"+ANSI_RESET+
-                    ANSI_CYAN+"                  2) Search for Student data by given ID                  \n"+ANSI_RESET+
-                     ANSI_PURPLE+ "                  -------------------------------------------                  \n"+ANSI_RESET+
-                    ANSI_CYAN+"                  3)  Edit Student Details                    \n"+ANSI_RESET+
-                    ANSI_PURPLE+ "                  -------------------------------------------                  \n"+ANSI_RESET+
-                    ANSI_CYAN+"                  4)  Delete Student Details                    \n"+ANSI_RESET+
-                    ANSI_PURPLE+ "                  -------------------------------------------                  \n"+ANSI_RESET+
-                    ANSI_CYAN+"                  5) Display All Students data                   \n"+ANSI_RESET+
-                     ANSI_PURPLE+ "                  -------------------------------------------                  \n"+ANSI_RESET
-                            + ANSI_CYAN+"                  6)   Save                  \n"
-                            + ANSI_RESET+ ANSI_PURPLE+"                  ---------------------                  \n"+ANSI_RESET
-                    +ANSI_CYAN+ "                  7)   EXIT                  \n"
-                            + ANSI_RESET+ ANSI_PURPLE+"                  ---------------------                  \n"+ANSI_RESET);
+                    CYAN+"                  1) Add Student data                  \n"+RESET
+                    + PURPLE+ "                  -------------------------------------------                  \n"+RESET+
+                    CYAN+"                  2) Search for Student data by given ID                  \n"+RESET+
+                    PURPLE+ "                  -------------------------------------------                  \n"+RESET+
+                    CYAN+"                  3)  Edit Student Details                    \n"+RESET+
+                   PURPLE+ "                  -------------------------------------------                  \n"+RESET+
+                   CYAN+"                  4)  Delete Student Details                    \n"+RESET+
+                    PURPLE+ "                  -------------------------------------------                  \n"+RESET+
+                    CYAN+"                  5) Display All Students data                   \n"+RESET+
+                     PURPLE+ "                  -------------------------------------------                  \n"+RESET
+                            + CYAN+"                  6)   Save                  \n"
+                            + RESET+ PURPLE+"                  ---------------------                  \n"+RESET
+                    +CYAN+ "                  7)   EXIT                  \n"
+                            + RESET+ PURPLE+"                  ---------------------                  \n"+RESET);
 
 
-            System.out.println(ANSI_BLUE+"\nEnter the choice : "+ ANSI_RESET);
+            System.out.println(BLUE+"\nEnter the choice : "+ RESET);
 
             int ch= sc.nextInt();
 
 
             switch (ch){
                 case 1:
-                    System.out.println(ANSI_YELLOW+"Enter the following details:  \n"+ ANSI_RESET);
-                    System.out.println(ANSI_YELLOW+"Enter ID:  "+ ANSI_RESET);
+                    //add
+                    System.out.println(YELLOW+"Enter the following details:  \n"+ RESET);
+                    System.out.println(YELLOW+"Enter ID:  "+ RESET);
                     id=sc.nextInt();
-                    System.out.println(ANSI_YELLOW+"Enter Name:  "+ ANSI_RESET);
+
+//                    System.out.println(YELLOW+" ID will be generate automatically:  "+ RESET);
+//                    id=rand.nextInt(1000-01) + 01;
+
+                    System.out.println(YELLOW+"Enter Name:  "+ RESET);
                     name=sc.next();
-                    System.out.println(ANSI_YELLOW+"Enter the Contact No:  "+ ANSI_RESET);
+                    System.out.println(YELLOW+"Enter the Contact No:  "+ RESET);
                     contact_no=sc.next();
-                    System.out.println(ANSI_YELLOW+"Enter email:  "+ ANSI_RESET);
+                    System.out.println(YELLOW+"Enter email:  "+RESET);
                     email=sc.next();
-                    System.out.println(ANSI_YELLOW+"Enter the Semester fee:  "+ ANSI_RESET);
+                    int w=0;
+                    try{
+                        for (int i=0; i<email.length();i++){
+                            if (email.contains("@")){
+                            w++    ;
+                            }
+                        }
+                     if (w==1)  {
+                         System.out.println(email);
+
+                     }
+                        if (w==0){
+                            System.out.println( RED+"\n----Not Correct, Please Enter the valid Email----"+RESET)  ;                     }
+                    }
+                    catch(Exception e){
+                        System.out.println(e);
+                    }
+                    System.out.println(YELLOW+"Enter the Semester fee:  "+ RESET);
                     fee=sc.nextDouble();
-                    emp.add(new Student(id,name,contact_no,email,fee));
-                    m.display(emp);
+                    Stud.add(new Student(id,name,contact_no,email,fee));
+                    m.display(Stud);
+                    try {
+                        fos=new FileOutputStream(f);
+                        oos=new ObjectOutputStream(fos);
+                        oos.writeObject(Stud);
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
                     break;
 
 
                 case 2: {
-                    System.out.println(ANSI_YELLOW+"Enter Student id to search: "+ ANSI_RESET);
+                    //search
+                    System.out.println(YELLOW+"Enter Student id to search: "+ RESET);
                     id = sc.nextInt();
                     int i = 0;
-                    for (Student e : emp) {
+                    for (Student e : Stud) {
                         if (id == e.id) {
                             System.out.println(e + "\n");
                             i++;
                         }
                     }
                     if (i == 0) {
-                        System.out.println(ANSI_RED+"Student ID is incorrect :( \n ** Please Enter valid ID!! "+ANSI_RESET);
+                        System.out.println(RED+"----Student ID is incorrect :( \n ** Please Enter valid ID!!----"+RESET);
 
                     }
                     break;
                 }
 
                 case 3:
-                    System.out.println(ANSI_YELLOW+"Enter Student Id to edit the details:  \n"+ ANSI_RESET);
+                    //Edit
+                    System.out.println(YELLOW+"Enter Student Id to edit the details:  \n"+ RESET);
                     id=sc.nextInt();
                     int j=0;
-                    for (Student e:emp) {
+                    for (Student e:Stud) {
                         if (id==e.id) {
                             j++;}
 
@@ -206,7 +246,24 @@ public class Student_Management {
                                 case 4:
                                 { System.out.println("Enter new Email:");
                                     e.email=sc.next();
-                                    System.out.println(e+"\n");
+                                    int q=0;
+                                    try{
+                                        for (int i=0; i<e.email.length();i++){
+                                            if (e.email.contains("@")){
+                                                q++ ;
+                                            }
+                                        }
+                                        if (q==1)  {
+                                            System.out.println(e.email);
+
+                                        }
+                                        if (q==0){
+                                            System.out.println( RED+"\n----Not Correct, Please Enter the valid Email----"+RESET)  ;
+                                        }
+                                    }
+                                    catch(Exception ep){
+                                        System.out.println(ep);
+                                    }
                                     break;}
                                 case 5:
                                 {  System.out.println("Enter new Semester fee:");
@@ -218,52 +275,47 @@ public class Student_Management {
                                 {j++;
                                     break;}
                                 default:
-                                { System.out.println(ANSI_RED+"Enter the correct choice!!"+ ANSI_RESET);
+                                { System.out.println(RED+"----Enter the correct choice!!----"+RESET);
                                     break;}
                             }
 
                         }}
                     if (j==0){
 
-                        System.out.println(ANSI_RED+"Student Details is not Found please enter the valid ID!!\n"+ANSI_RESET);
+                        System.out.println(RED+"----Student Details is not Found please enter the valid ID!!----\n"+RESET);
                     }
                     break;
 
                 case 4:
-                    System.out.println(ANSI_RED+"Enter the Student ID to delete the Data"+ANSI_RESET);
+                    //DELETE
+                    System.out.println(RED+"Enter the Student ID to delete the Data"+RESET);
                     id= sc.nextInt();
                     int k=0;
                     try {
-                        for (Student e:emp){
+                        for (Student e:Stud){
                             if (id==e.id){
-                                emp.remove(e);
+                                Stud.remove(e);
                                 k++;
                             }
 
                         }if (k==0){
-                            System.out.println(ANSI_RED+"Student details is not available.\n Please enter the valid ID\n"+ANSI_RESET);
+                            System.out.println(RED+"----Student details is not available.\n Please enter the valid ID----\n"+RESET);
                         }
                     }catch (Exception ex){
-                        System.out.println(ex);
+                        System.out.println(RED+"\n---- Deleting the file ----"+RESET);
                     }
 
 
                 case 5:
-                    try {
-                        emp=(ArrayList<Student>) ois.readObject();
-                    }catch (IOException ex){
-
-                    }catch (Exception e){
-
-                    }
-
-                    m.display(emp);
+                    m.display(Stud);
                     break;
                 case 6:
+
+                    //save
                     try {
                         fos=new FileOutputStream(f);
                         oos=new ObjectOutputStream(fos);
-                        oos.writeObject(emp);
+                        oos.writeObject(Stud);
                     }catch (IOException e){
                         System.out.println(e);
                     }
@@ -278,9 +330,7 @@ public class Student_Management {
                         }catch (IOException ex){
                             System.out.println(ex);
                         }
-                        System.out.println(ANSI_RED+"\n Saving the file "+ANSI_RESET);
-
-
+                        System.out.println(RED+"\n Saving the file "+RESET);
                         break;
 
                     } // Finally block end
@@ -293,12 +343,8 @@ public class Student_Management {
                     break;
                 }
                 default:
-                    System.out.println(ANSI_RED+"\n Enter the correct choice from given options"+ANSI_RESET);
+                    System.out.println(RED+"\n----Enter the correct choice from given options----"+RESET);
             }
-
         }while (true);
-
-
     }
-
 }
